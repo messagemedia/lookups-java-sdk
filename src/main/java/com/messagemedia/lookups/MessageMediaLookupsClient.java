2514a70b6e84b19c03e5a0e3beb6b118f1ffdb30
coder_bec <bec@coderbec.comm>
2019-02-22 11:17:30 +1100

    Revert "v 1.0.1 now has constructor for HMAC authentication"
    This reverts commit c59b061d69021d93eccfd8e7a6cf0ac9e11c0faf.

--- src/main/java/com/messagemedia/lookups/MessageMediaLookupsClient.java
+++ src/main/java/com/messagemedia/lookups/MessageMediaLookupsClient.java
@@ -40,18 +40,9 @@ public class MessageMediaLookupsClient {
     /**
      * Client initialization constructor
      */
-    public MessageMediaLookupsClient(String authUserName, String authPassword, Boolean HMAC) {
+    public MessageMediaLookupsClient(String basicAuthUserName, String basicAuthPassword) {
         this();
-        
-        if (HMAC) {
-        	Configuration.hmacAuthUserName = authUserName;
-        	Configuration.hmacAuthPassword = authPassword;
-        } else {
-        	Configuration.basicAuthUserName = authUserName;
-            Configuration.basicAuthPassword = authPassword;
-        }
-        
+        Configuration.basicAuthUserName = basicAuthUserName;
+        Configuration.basicAuthPassword = basicAuthPassword;
     }
-    
- 
 }
